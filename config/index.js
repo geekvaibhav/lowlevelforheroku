6afc869f63dc7a475136f0b9f6f66211fd11dbcc
Vaibhav <geekvaibhav@users.noreply.github.com>
2017-03-08 16:06:45 +0530

    running Bot Upload

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const config = {}
 
 config.recastToken = 'dccef3e283b71479163b3f8edad2e181'
-config.botToken = '0c5e71c6c192e5af5789e5c429ff45350c5e71c6c192e5af5789e5c429ff4535'
+config.botToken = '0c5e71c6c192e5af5789e5c429ff4535'
 config.language = 'en'
 config.url= 'https://api-botconnector.recast.ai'
 config.botId = '7df85da2-b550-46a6-987d-b199dec738da'
